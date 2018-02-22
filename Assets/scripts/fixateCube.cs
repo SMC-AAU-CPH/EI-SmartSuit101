df8b1cc7a089ae269b7a96d3a95969e48571bc57
cerkut <Cumhur.Erkut@gmail.com>
2018-02-22 09:54:50 +0100

    Repo initialization

---
+++ Assets/scripts/fixateCube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fixateCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  GameObject Guy = GameObject.Find("RokokoGuy");
+      GameObject Spine = GameObject.Find("RokokoGuy_Spine");
+      GameObject Box = GameObject.Find("Box");
+
+      Vector3 GuySize = Guy.GetComponent<Renderer>().bounds.size;
+
+      Box.transform.localScale = GuySize;
+
+      Box.transform.position = Spine.transform.position;
+      Box.transform.rotation = Spine.transform.rotation;
+ 
+	}
+}
